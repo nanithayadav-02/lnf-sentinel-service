2097d0e88fb6e6bbcc1b28efbbabf14f57d33036
sivaSeshaReddy <siva.s@leverandfulcrum.com>
2026-07-01 10:28:12 +0530

    url correction

--- src/main/java/com/lnf/sentinel/controller/IssueWatcherController.java
+++ src/main/java/com/lnf/sentinel/controller/IssueWatcherController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/sentinel/issues")
+@RequestMapping("lnf/sentinel/issues")
 @RequiredArgsConstructor
 public class IssueWatcherController {
 
